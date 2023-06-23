31660220c5222acb46755e680b95384843561b88
Abhishek <abhishekgupta974@yahoo.com>
2023-06-23 14:06:19 +0530

    loops

---
+++ t2.py
@@ -0,0 +1,27 @@
+# 1. 
+
+"""from turtle import*
+
+size=10
+while True:
+    fd(size)
+    lt(60)
+    size+=1
+"""
+
+
+
+
+# 2.
+
+""" from turtle import*
+speed('fastest')
+size=10
+angle=61
+colors=['red','purple','yellow','violet','indigo','green','blue','orange']
+while True:
+    pencolor(colors[size%8])
+    fd(size)
+    lt(angle)
+    size+=1
+"""
